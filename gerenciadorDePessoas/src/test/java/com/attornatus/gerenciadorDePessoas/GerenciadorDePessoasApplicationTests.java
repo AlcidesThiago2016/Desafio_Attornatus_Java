95286820bcd9400288b2555ad90b273a88256393
alcid <alcides_thiago@hotmail.com>
2023-02-15 15:03:00 -0300

    Initial commit

---
+++ gerenciadorDePessoas/src/test/java/com/attornatus/gerenciadorDePessoas/GerenciadorDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.gerenciadorDePessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
