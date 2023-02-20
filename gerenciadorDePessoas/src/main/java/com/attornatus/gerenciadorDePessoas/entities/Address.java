6ef97f032893c04bf4db1913237c61d40b5f9239
alcid <alcides_thiago@hotmail.com>
2023-02-19 22:19:05 -0300

    implementation class Address.java and Person.java.

--- gerenciadorDePessoas/src/main/java/com/attornatus/gerenciadorDePessoas/entities/Address.java
+++ gerenciadorDePessoas/src/main/java/com/attornatus/gerenciadorDePessoas/entities/Address.java
@@ -1,4 +1,43 @@
 package com.attornatus.gerenciadorDePessoas.entities;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
 
-public class Address {
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "address")
+@Entity
+public class Address implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String place;
+
+    private int zipCode;
+
+    private String number;
+
+    private String city;
+
+    @JsonManagedReference
+    @ManyToOne
+    private Person person;
+
+    public Address(String place, int zipCode, String number, String city, Person person) {
+        this.place = place;
+        this.zipCode = zipCode;
+        this.number = number;
+        this.city = city;
+        this.person = person;
+    }
 }
--- gerenciadorDePessoas/src/main/java/com/attornatus/gerenciadorDePessoas/entities/Person.java
+++ gerenciadorDePessoas/src/main/java/com/attornatus/gerenciadorDePessoas/entities/Person.java
@@ -1,4 +1,42 @@
 package com.attornatus.gerenciadorDePessoas.entities;
 
-public class Person {
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "person")
+@Entity
+public class Person implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Id
+    @Column(name = "date_birth")
+    private Date dateOfBirth;
+
+    @JsonManagedReference
+    @OneToOne(mappedBy = "person", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Address> addresses = new ArrayList<>();
+
+    public Person(String name, Date dateOfBirth){
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
 }
